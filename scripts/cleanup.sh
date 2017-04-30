023ba9081564bb63bb78e88ad84316b674e7bdad
Serghei Iakovlev <serghei@phalconphp.com>
2017-04-30 09:31:46 +0300

    Initial provision

---
+++ scripts/cleanup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -eux
+
+# Apt cleanup.
+apt -y autoremove
+apt -y autoclean
+
+# Try to upgrade
+apt -y update
+apt -y upgrade
+
+# Delete unneeded files
+rm -rf \
+	/root/{.bash_history,daemonize*,.composer,.npm,.wget-hsts} \
+	/home/vagrant/{.ansibl*,.bash_history,.sudo*,.wget-hsts,.cache/*,*.sh,.v8*} \
+	/var/lib/apt/lists/* \
+	/tmp/* \
+	/var/tmp/* \
+	/etc/apt/{sources.list~,trusted.gpg~} \
+	/var/cache/apt/archives/* \
+	/var/cache/debconf/*-old
+
+# Workaround
+rm -f /usr/local/bin/bash
+
+# Clean logs
+dd if=/dev/null of=/var/log/syslog
+
+cat /dev/null > /home/vagrant/.bash_history
+
+unset HISTFILE
+
+# Zero out the rest of the free space using dd, then delete the written file.
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
+
+# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+sync
---
+++ scripts/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -eux
+
+apt -y update
+
+apt -y install software-properties-common curl
+
+apt-add-repository ppa:ansible/ansible -y > /dev/null 2>&1
+
+apt-add-repository ppa:ondrej/php -y > /dev/null 2>&1
+echo "deb http://ppa.launchpad.net/ondrej/php/ubuntu xenial main/debug" | tee -a /etc/apt/sources.list.d/ondrej-ubuntu-php-xenial.list
+echo "deb-src http://ppa.launchpad.net/ondrej/php/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/ondrej-ubuntu-php-xenial.list
+
+apt -y update
+apt-get -y upgrade
+
+apt-get -y install ansible
+
+# echo "LC_ALL=en_US.UTF-8" >> /etc/default/locale
+# locale-gen en_US.UTF-8
+
+# Add vagrant user to sudoers.
+echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
