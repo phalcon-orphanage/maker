1b4fc4a077c5c4b965f6e3e2eef7c87d29be8a31
Serghei Iakovlev <serghei@phalconphp.com>
2017-10-07 19:23:33 +0300

    Cleanup cleanup.sh ;)

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,31 +1,30 @@
 #!/bin/bash -eux
 
-export export DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 
-function print_info {
-	echo -e "\033[1;33m${1}\033[0m"
-}
+apt-get -y autoremove
+apt-get -y clean
 
-print_info 'Cleanup log files'
-find /var/log -type f | while read f; do echo -ne '' > $f; done
+echo 'Cleanup log files'
+find /var/log -type f | while read f; do echo -ne '' > $f 2&>1 > /dev/null; done
 
-print_info 'Whiteout swap'
+echo 'Whiteout swap'
 swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
 swapoff $swappart
-dd if=/dev/zero of=$swappart
+dd if=/dev/zero of=$swappart || true
 mkswap -f $swappart
 swapon $swappart
 
-print_info 'Cleanup bash history'
+echo 'Cleanup bash history'
 unset HISTFILE
 [ -f /root/.bash_history ] && rm /root/.bash_history
 [ -f /home/vagrant/.bash_history ] && rm /home/vagrant/.bash_history
 
-print_info 'Zero out the rest of the free space using dd, then delete the written file'
-dd if=/dev/zero of=/EMPTY bs=1M
+echo 'Zero out the rest of the free space using dd, then delete the written file'
+dd if=/dev/zero of=/EMPTY bs=1M || true
 rm -f /EMPTY
 
-print_info "Add `sync` so Packer doesn't quit too early, before the large file is deleted"
+echo "Add `sync` so Vagrant doesn't quit too early, before the large file is deleted"
 sync
 
-print_info 'Vagrant cleanup complete!'
+echo 'Vagrant cleanup complete!'
